9cc1f2a2274e70211781198b73caf08794511ab7
bsd <datta.bathina15@gmail.com>
2020-07-11 22:31:07 +0530

    Made container center by default

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 module.exports = {
-  purge: [],
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
-}
+	purge: [],
+	theme: {
+		extend: {},
+		container: {
+			padding: {
+				default: "1rem",
+				sm: "2rem",
+				lg: "4rem",
+				xl: "5rem",
+			},
+		},
+	},
+	variants: {},
+	plugins: [],
+};
