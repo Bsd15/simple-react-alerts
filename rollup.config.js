8868627f82a95dcb7ffa1bfc6c1aac39e2fd0a7d
bsd <datta.bathina15@gmail.com>
2020-07-11 23:29:45 +0530

    Added babel to plugins

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import postcss from "rollup-plugin-postcss";
+import babel from "@rollup/plugin-babel";
 
 const packageJson = require("./package.json");
 
@@ -27,5 +28,6 @@ export default {
 			modules: true,
 			plugins: [],
 		}),
+		babel({ babelHelpers: "bundled" }),
 	],
 };
